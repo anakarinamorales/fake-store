f9aec80314ad45f585215b8609b5b093c52be1dd
Ana Karina M. Machado <akarinammachado@gmail.com>
2024-10-20 21:26:09 +0200

    💄 Adjusted homepage styles. Removed unused CSS. Uploaded new favicon.

---
+++ src/components/pageContainer/pageContainer.styled.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+    display: flex;
+    flex-direction: column;
+    gap: 2em;
+    height: 100%;
+    margin: auto;
+    width: 70%;
+    align-items: center;
+    padding: 3em 0;
+`;
