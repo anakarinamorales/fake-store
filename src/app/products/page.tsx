6f2123c0102d7a6dc1501ffa76b7fe9d4295e5bc
Ana Karina M. Machado <akarinammachado@gmail.com>
2025-02-11 19:42:59 +0100

    CHanged PLP style

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,6 +3,7 @@ import cardStyles from './page.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
 import { getProducts } from '@/api';
+import Card from 'components/Card';
 
 export default async function Products() {
     const products = await getProducts();
@@ -10,23 +11,28 @@ export default async function Products() {
     return (
         <main>
             <h1>Products</h1>
+
             <ul className={cardStyles.cardList}>
-                {products.map((item: Product) => (
-                    <li className={cardStyles.cardItem} key={item?.id}>
-                        <Image
-                            src={item?.image}
-                            width={300}
-                            height={340}
-                            alt="Product image"
-                        />
-                        <h2>
-                            <Link
-                                className={cardStyles.cardLink}
-                                href={`products/${item?.id}`}
-                            >
-                                {item?.title}
-                            </Link>
-                        </h2>
+                {products.map((item: Product, index: number) => (
+                    <li key={item?.id}>
+                        <Link
+                            className={cardStyles.cardLink}
+                            href={`products/${item?.id}`}
+                        >
+                            <Card
+                                content={
+                                    <Image
+                                        src={item?.image}
+                                        width={200}
+                                        height={215}
+                                        alt="Product image"
+                                        priority={index < 8}
+                                    />
+                                }
+                                title={item?.title}
+                                tags={[item?.category]}
+                            />
+                        </Link>
                     </li>
                 ))}
             </ul>
